ab8cd7fee1dff9b4da7e30ed0ce22a4483305491
Dmitrii <chernishow.dima@yandex.ru>
2019-10-30 20:54:40 +0300

    first start

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pymongo
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+client = pymongo.MongoClient("mongodb+srv://admin:adminqwerty@cookerybook-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
+
+cookery_database = client["cookerybook"]
+cookery_list_collection = cookery_database["cookery-list"]
+
+
+@app.route('/')
+@app.route('/index')
+def main():
+    return jsonify(', '.join([str(item.get('text')) for item in cookery_list_collection.find()]))
