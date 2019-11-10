a1100d671006d177b79f80a02ad506e8cdf24012
Dmitrii <chernishow.dima@yandex.ru>
2019-11-10 15:29:57 +0300

    Name of project has been changed

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-client = pymongo.MongoClient("mongodb+srv://admin:adminqwerty@cookerybook-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient("mongodb+srv://admin:adminqwerty@book-of-recipe-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
-cookery_database = client["cookerybook"]
-cookery_list_collection = cookery_database["cookery-list"]
+
+book_of_recipes_database = client["book-of-recipe"]
+book_of_recipes_collection = cookery_database["recipes"]
 
 
 @app.route('/')
 @app.route('/index')
 def main():
-    return jsonify(', '.join([str(item.get('text')) for item in cookery_list_collection.find()]))
+    return jsonify(', '.join([str(item.get('text')) for item in book_of_recipes_collection.find()]))
