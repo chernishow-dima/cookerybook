cb6f0a3d98fbfedf3b088faf734cbf1a7175c18a
Dmitrii <chernishow.dima@yandex.ru>
2019-11-18 20:42:29 +0300

    add adding of recipe

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pymongo
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-
 client = pymongo.MongoClient("mongodb+srv://admin:adminqwerty@book-of-recipe-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
-
 
 book_of_recipes_database = client["book-of-recipe"]
 book_of_recipes_collection = book_of_recipes_database["recipes"]
@@ -17,3 +15,27 @@ book_of_recipes_collection = book_of_recipes_database["recipes"]
 @app.route('/index')
 def main():
     return jsonify(', '.join([str(item.get('text')) for item in book_of_recipes_collection.find()]))
+
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == 'POST':
+        name = request.get_json()["name"]
+        category = request.get_json()["category"]
+        ingredients = request.get_json()["ingredients"]
+        calorie = request.get_json()["calorie"]
+        recipe = request.get_json()["recipe"]
+        celebratory = request.get_json()["celebratory"]
+        photo_url = request.get_json()["photo"]
+        like = 0
+        book_of_recipes_collection.insert_one({'name': name,
+                                               'category': category,
+                                               'ingredients': ingredients,
+                                               'calorie': calorie,
+                                               'recipe': recipe,
+                                               'celebratory': celebratory,
+                                               'photo': photo_url,
+                                               'like': like})
+        return "Рецепт добавлен"
+    return jsonify(', '.join([str(item) for item in book_of_recipes_collection.find()]))
+
