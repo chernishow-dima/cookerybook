ddb2761fd468711333a31f7c560e1f71ccc9cb4b
Dmitrii <chernishow.dima@yandex.ru>
2019-11-19 17:57:10 +0300

    add edit of recipe

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pymongo
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-client = pymongo.MongoClient("mongodb+srv://admin:adminqwerty@book-of-recipe-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://admin:adminqwerty@book-of-recipe-mx8jr.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
 book_of_recipes_database = client["book-of-recipe"]
@@ -14,28 +16,50 @@ book_of_recipes_collection = book_of_recipes_database["recipes"]
 @app.route('/')
 @app.route('/index')
 def main():
-    return jsonify(', '.join([str(item.get('text')) for item in book_of_recipes_collection.find()]))
+    return jsonify(', '.join([str(item) for item in book_of_recipes_collection.find()]))
+
+
+@app.route('/catalog')
+def show_catalog():
+    return jsonify(', '.join([str(item) for item in book_of_recipes_collection.find()]))
 
 
 @app.route('/add_recipe', methods=['GET', 'POST'])
 def add_recipe():
     if request.method == 'POST':
-        name = request.get_json()["name"]
-        category = request.get_json()["category"]
-        ingredients = request.get_json()["ingredients"]
-        calorie = request.get_json()["calorie"]
-        recipe = request.get_json()["recipe"]
-        celebratory = request.get_json()["celebratory"]
-        photo_url = request.get_json()["photo"]
-        like = 0
-        book_of_recipes_collection.insert_one({'name': name,
-                                               'category': category,
-                                               'ingredients': ingredients,
-                                               'calorie': calorie,
-                                               'recipe': recipe,
-                                               'celebratory': celebratory,
-                                               'photo': photo_url,
-                                               'like': like})
+        book_of_recipes_collection.insert_one({'name': request.get_json()["name"],
+                                               'category': request.get_json()["category"],
+                                               'ingredients': request.get_json()["ingredients"],
+                                               'calorie': request.get_json()["calorie"],
+                                               'recipe': request.get_json()["recipe"],
+                                               'celebratory': request.get_json()["celebratory"],
+                                               'photo': request.get_json()["photo"],
+                                               'like': 0})
         return "Рецепт добавлен"
-    return jsonify(', '.join([str(item) for item in book_of_recipes_collection.find()]))
+    return redirect('/')
 
+
+@app.route('/edit_recipe', methods=['GET', 'POST'])
+def edit_recipe():
+    if request.method == 'POST':
+        print([i for i in book_of_recipes_collection.find({"_id": ObjectId(request.get_json()["_id"])})])
+        print(request.get_json()["_id"])
+        result = book_of_recipes_collection.update_one({
+            "_id": ObjectId(request.get_json()["_id"])
+
+        }, {
+            "$set": {'name': request.get_json()["name"],
+                     'category': request.get_json()["category"],
+                     'ingredients': request.get_json()["ingredients"],
+                     'calorie': request.get_json()["calorie"],
+                     'recipe': request.get_json()["recipe"],
+                     'celebratory': request.get_json()["celebratory"],
+                     'photo': request.get_json()["photo"],
+                     'like': request.get_json()["like"]
+                     }}, upsert=False)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
