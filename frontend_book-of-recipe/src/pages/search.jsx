9c47b50c9e33329d2b6bb1d182934e6bcd79f4a1
Egorushkova <56114426+Egorushkova@users.noreply.github.com>
2019-12-15 22:32:36 +0300

    edit search page

--- frontend_book-of-recipe/src/pages/search.jsx
+++ frontend_book-of-recipe/src/pages/search.jsx
@@ -1,9 +1,89 @@
-import React, { Component } from 'react';
-export default class Search extends Component {
-  state = {}
-  render() {
-    return (
-      <h1>Страница поиска</h1>
-    );
-  }
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import image_p from '../img/pancakes.jpg';
+import image_m from '../img/meet.jpg';
+
+const tileData = [
+  {
+    img: image_p,
+    title: 'Блинчики',
+  },
+  {
+    img: image_m,
+    title: 'Мясо',
+  },
+  {
+    img: image_p,
+    title: 'Блинчики',
+  },
+  {
+    img: image_m,
+    title: 'Мясо',
+  },
+  {
+    img: image_p,
+    title: 'Блинчики',
+  },
+  {
+    img: image_m,
+    title: 'Мясо',
+  },
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 950,
+    height: 500,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
+
+export default function TitlebarGridList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cols={3} cellHeight={200} spacing={50} className={classes.gridList}>
+        {tileData.map(tile => (
+            <GridListTile key={tile.img}>
+              <img src={tile.img} alt={tile.title}/>
+              <Link to="/">
+                <GridListTileBar
+                  title={tile.title}
+                  actionIcon={
+                    <Link to="/search">
+                      <IconButton color={"secondary"} >&#10084;</IconButton>
+                    </Link>
+                  }
+                />
+              </Link>
+            </GridListTile> 
+        ))}
+      </GridList>
+    </div>
+  );
 }
+
+
+
+/**
+
+        <GridListTile key="Subheader" cols={4} style={{ height: 'auto' }}>
+          <ListSubheader component="div">Результаты поиска</ListSubheader>
+        </GridListTile>
+
+*/
